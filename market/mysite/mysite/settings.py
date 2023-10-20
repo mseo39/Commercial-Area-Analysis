7a57baae1a550c6743c6a19b09333447d511fe4e
mseo39 <a01063294905@gmail.com>
2023-10-20 14:50:55 +0000

    add cors

--- market/mysite/mysite/settings.py
+++ market/mysite/mysite/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'main.apps.MainConfig',
     'django_celery_beat',
     'django_celery_results',
+    'corsheaders',
 ]
 
 # Celery 설정
@@ -52,6 +53,9 @@ CELERY_RESULT_SERIALIZER = 'json'
 
 
 MIDDLEWARE = [
+    # CORS
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -59,14 +63,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # CORS
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    
 ]
 
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
-    'http://localhost:8081',
+    'http://220.69.209.126:8088',
 )
 
 ROOT_URLCONF = 'mysite.urls'
