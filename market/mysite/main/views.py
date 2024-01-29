fec0734f621d00c55c3fba8ae66c0ce92437d24c
2.8 <a01063294905@gmail.com>
2024-01-29 20:13:29 +0900

    Update views.py

--- market/mysite/main/views.py
+++ market/mysite/main/views.py
@@ -21,27 +21,42 @@ logger = logging.getLogger(__name__)
 #파일 업로드 기능
 @api_view(['POST'])
 def upload_file(request):
+    # POST 메서드로 요청이 들어온 경우에만 처리
     if request.method == 'POST':
+        # 클라이언트에서 전송한 데이터 가져오기
         name = request.POST['name']
-        country=request.POST["country"]
-        city=request.POST["city"]
+        country = request.POST["country"]
+        city = request.POST["city"]
+
+        # 로그에 나라와 도시 정보 출력
         logger.error(country)
         logger.error(city)
 
-        file_data={}
+        # 업로드된 파일들의 데이터를 저장할 딕셔너리 초기화
+        file_data = {}
+        # 전송된 파일들을 반복하여 처리
         for i in name.split(","):
-            if i!="":
+            if i != "":
                 logger.error(i)
+                # 클라이언트에서 전송된 파일 중 하나를 선택하여 업로드된 파일 객체를 가져옴
                 uploadFile = request.FILES[i]
+                # 파일 내용을 UTF-8 형식으로 디코딩하면서 BOM(Byte Order Mark) 문자열 제거
+                # BOM 문자열은 텍스트 파일의 처음에 위치하며, 파일이 어떤 인코딩 방식을 사용하는지 나타냄
                 read = uploadFile.read().decode('utf-8-sig')
+                # 읽어들인 파일 내용을 개행 문자('\r\n')를 기준으로 분리하여 리스트로 저장
+                # Windows 환경에서의 개행 문자는 '\r\n'
                 readLine = read.split('\r\n')
-                file_data[i]=readLine
+                # 파일 데이터 딕셔너리에 파일 이름(key)과 내용(value)을 저장
+                file_data[i] = readLine
 
-        process_uploaded_file.delay(file_data,country,city)
+        # 비동기적으로 파일 처리 작업 시작
+        process_uploaded_file.delay(file_data, country, city)
 
-        return JsonResponse({'message': '파일 업로드 작업이 시작되었습니다.'}, status=status.HTTP_202_ACCEPTED)
+        # JsonResponse 메시지 수정 및 응답 상태코드 지정
+        return JsonResponse({'message': '파일 업로드 작업이 시작되었습니다.', 'status': 'ACCEPTED'}, status=status.HTTP_202_ACCEPTED)
 
-    return JsonResponse({'message': 'Tutorials were deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
+    # POST 요청이 아닌 경우에는 다른 응답을 반환
+    return JsonResponse({'message': '올바르지 않은 요청입니다.'}, status=status.HTTP_400_BAD_REQUEST)
 
 from django.http import JsonResponse
 from main.models import Store_data, Revenue_data, Apart_data, Population_data, Commercial_data
